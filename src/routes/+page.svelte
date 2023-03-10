e670f6b0bd1aacb4dc2ad4e1040ff69c536b40fe
hethw <gowheth@gmail.com>
2023-03-10 12:03:33 -0800

    tailwindcss added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -16,6 +16,9 @@
 />
 
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+    Button
+</button>
 
 <Accordion>
     <AccordionItem title="Section 1" open> Content 1</AccordionItem>
